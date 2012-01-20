3139eb6253d618437eaff64b36e6eb390ab67fb5
Rollen Gomes <avelliance@gmail.com>
2012-01-20 11:25:13 +0800

    Initial Commit

---
+++ RGMoviePlayer/RGMoviePlayerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RGMoviePlayerAppDelegate.h
+//  RGMoviePlayer
+//
+//  Created by ROLLEN GOMES on 1/20/12.
+//  Copyright (c) 2012 NUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGMoviePlayerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RGMoviePlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RGMoviePlayer
+//
+//  Created by ROLLEN GOMES on 1/20/12.
+//  Copyright (c) 2012 NUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RGMoviePlayerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RGMoviePlayerAppDelegate class]));
+    }
+}
